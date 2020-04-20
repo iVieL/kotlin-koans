450bef029c6094afc44f5025990acd5dc4a99014
vlutin <victor.lutin@gmail.com>
2020-04-20 15:31:30 -0600

    patitioning example, my solution was a little expensive, I used the suggested

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,7 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> =
+        customers.filter {
+            val (delivered, undelivered) = it.orders.partition { it.isDelivered }
+            undelivered.size > delivered.size
+        }.toSet()
+//customers.partition { it -> it.orders.sumBy { if (it.isDelivered) 1 else 0 } < it.orders.sumBy { if(!it.isDelivered) 1 else 0 }}.component1().toSet()
