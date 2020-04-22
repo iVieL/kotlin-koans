738108f03a22645395ba11ba54df6fdcabddc7ac
vlutin <victor.lutin@gmail.com>
2020-04-22 11:36:12 -0600

    a builder with function literals, cool!

--- Builders/String and map builders/src/Task.kt
+++ Builders/String and map builders/src/Task.kt
@@ -1,6 +1,10 @@
 import java.util.HashMap
 
-/* TODO */
+fun buildMap(map: HashMap<Int, String>.() -> Unit): HashMap<Int, String> {
+    val mapBuilder = HashMap<Int, String>()
+    mapBuilder.map()
+    return mapBuilder
+}
 
 fun usage(): Map<Int, String> {
     return buildMap {
